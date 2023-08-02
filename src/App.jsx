c438f347300a1c18a0d918dfbae6d8d95ae70f4a
jonytellodev <jonathantello.dev@gmail.com>
2023-08-02 11:28:49 -0300

    creacion archivos iniciales de proyecto y maquetacion navbar y footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/common/Footer';
+import LogoHeader from './components/common/LogoHeader';
+
+function App() {
+
+  return (
+    <>
+    <LogoHeader></LogoHeader>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/common/LogoHeader.jsx
@@ -0,0 +1,11 @@
+const LogoHeader = () => {
+  return (
+    <>
+      <nav class="bgNavTW d-flex justify-content-center p-4">
+        <img src="public\logoTW.svg" alt="logoTW" />
+      </nav>
+    </>
+  );
+};
+
+export default LogoHeader;
